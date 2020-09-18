bc11672e9f815a8196a90aab39a3c16f66fa3d86
Xu Wei <1341890034@qq.com>
2020-09-18 11:49:45 +0800

    测试接口

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.get("/test", (req, res) => {
+	res.setHeader("Content-Type", "text/html;charset=utf-8");
+	res.end("如何评价《英雄联盟》S10 主题曲？");
+});
+
 app.listen("8000", function (err) {
 	if (err) {
 		console.log("服务启动失败");
