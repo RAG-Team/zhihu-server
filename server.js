1978a0b59cadc997994722834c103740e258073d
xurenqiang <526796576@qq.com>
2020-09-21 19:36:42 +0800

    第一次提交

--- server.js
+++ server.js
@@ -6,19 +6,26 @@ const app = new Koa()
 const router = new KoaRouter()
 app.use(router.routes())
 	.use(router.allowedMethods())
-router.get("/test", (ctx,next) => {
+router.get("/test", (ctx, next) => {
 	// res.setHeader("Content-Type", "text/html;charset=utf-8");
-  // res.end("如何评价《英雄联盟》S10 主题曲？");
-  const string = "如何评价《英雄联盟》S10 主题曲？"
-  ctx.body=string
+	// res.end("如何评价《英雄联盟》S10 主题曲？");
+	const string = "如何评价《英雄联盟》S10 主题曲？"
+	ctx.body = string
 });
 
 // 等你来答数据请求
-const  questionDatas = require('./datas/WaitYou.json')
-router.get("/question",(ctx,next)=>{
-  // res.setHeader("Content-Type", "text/html;charset=utf-8");
-  // res.end(questionDatas);
-  ctx.body = questionDatas
+const questionDatas = require('./datas/WaitYou.json')
+router.get("/question", (ctx, next) => {
+	// res.setHeader("Content-Type", "text/html;charset=utf-8");
+	// res.end(questionDatas);
+	ctx.body = questionDatas
+})
+// 我的主页数据
+const myHomeDatas = require('./datas/MyHome.json')
+router.get("/myhome", (ctx, next) => {
+	// res.setHeader("Content-Type", "text/html;charset=utf-8");
+	// res.end(questionDatas);
+	ctx.body = myHomeDatas
 })
 
 app.listen("6666", function (err) {
