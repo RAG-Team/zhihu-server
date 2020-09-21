0680d7cf229e021516d7db6592dfd0df96fbc56a
Xu Wei <1341890034@qq.com>
2020-09-21 20:48:45 +0800

    登陆界面数据请求

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const KoaRouter = require('koa-router')
 // const app = express();
 const app = new Koa()
 const router = new KoaRouter()
+// 使用中间件
 app.use(router.routes())
-	.use(router.allowedMethods())
+  .use(router.allowedMethods())
+  // 测试接口
 router.get("/test", (ctx,next) => {
 	// res.setHeader("Content-Type", "text/html;charset=utf-8");
   // res.end("如何评价《英雄联盟》S10 主题曲？");
@@ -19,6 +21,11 @@ router.get("/question",(ctx,next)=>{
   // res.setHeader("Content-Type", "text/html;charset=utf-8");
   // res.end(questionDatas);
   ctx.body = questionDatas
+})
+// 手机号登录数据请求
+const loginInfoList = require('./datas/login.json')
+router.post("/login",(ctx,next)=>{
+  ctx.body = loginInfoList
 })
 
 app.listen("6666", function (err) {
