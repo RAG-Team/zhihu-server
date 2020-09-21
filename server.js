6a085d2a6479279bfdb4747e4a6776cefa18aebb
Xu Wei <1341890034@qq.com>
2020-09-21 09:14:39 +0800

    改变端口

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ app.get("/test", (req, res) => {
 	res.end("如何评价《英雄联盟》S10 主题曲？");
 });
 
-app.listen("8000", function (err) {
+
+app.listen("6666", function (err) {
 	if (err) {
 		console.log("服务启动失败");
 	} else {
-		console.log("服务启动成功，8000端口监听中");
+		console.log("服务启动成功，6666端口监听中");
 	}
 });
