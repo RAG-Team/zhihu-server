da7170653184c10e33ed42b86ce9d8291ce11ab4
Xu Wei <1341890034@qq.com>
2020-09-21 09:49:05 +0800

    koa修改

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/test", (req, res) => {
-	res.setHeader("Content-Type", "text/html;charset=utf-8");
-	res.end("如何评价《英雄联盟》S10 主题曲？");
+// const express = require("express");
+const Koa = require('koa')
+const KoaRouter = require('koa-router')
+// const app = express();
+const app = new Koa()
+const router = new KoaRouter()
+app.use(router.routes())
+	.use(router.allowedMethods())
+router.get("/test", (ctx,next) => {
+	// res.setHeader("Content-Type", "text/html;charset=utf-8");
+  // res.end("如何评价《英雄联盟》S10 主题曲？");
+  const string = "如何评价《英雄联盟》S10 主题曲？"
+  ctx.body=string
 });
 
+// 等你来答数据请求
+const  questionDatas = require('./datas/WaitYou.json')
+router.get("/question",(ctx,next)=>{
+  // res.setHeader("Content-Type", "text/html;charset=utf-8");
+  // res.end(questionDatas);
+  ctx.body = questionDatas
+})
 
 app.listen("6666", function (err) {
 	if (err) {
